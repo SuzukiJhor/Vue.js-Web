033f2cdb0b5d2ac411834eca2be94e49a87adfcf
Jhordan Kendy Suzuki <jhordan.suzuki@followize.local>
2023-07-15 14:40:21 -0300

    feat: Inserindo axios e métodos de consulta a api

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,18 +1,32 @@
 <template>
     <footer>
-
+        <a class="socialLink" v-for="(socialLink, index) in socialLinks" :key="index" :href="socialLink.link">
+            <img :src="socialLink.icon" :alt="socialLink.label" :title="socialLink.label">
+        </a>
     </footer>
 </template>
 
 <script>
+import api from '@/services/api.js'
+
 export default {
-    name: "FooterComponent"
+    name: "FooterComponent",
+    data() {
+        return {
+            socialLinks: []
+        }
+    },
+    mounted() {
+        api.get('/social-links.json').then(res => {
+            this.socialLinks = res.data;
+        });
+    }
+
 }
 </script>
 
 <style scoped>
-
-footer{
+footer {
     width: 100%;
     height: 60px;
     background-color: var(--color-background-nav);
@@ -21,4 +35,19 @@ footer{
     align-items: center;
 }
 
+.socialLink {
+    border: 1px solid var(--color-text-light);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    width: 40px;
+    height: 40px;
+    margin: 0 10px;
+}
+
+.socialLink img {
+    width: 20px;
+
+}
 </style>
